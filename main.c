dec06f1d9499424de9cc1c2eb111b7ccf46c8c57
“cpmoore0317” <kingpkayy@gmail.com>
2023-08-10 09:12:15 -0700

    Adding the README file, a header file with the structs, and a main file with a basic functionality for opening, reading lines, and closing files

---
+++ main.c
@@ -0,0 +1,53 @@
+#include "monty.h"
+
+/**
+ */
+int main(int argc, char **argv)
+{
+	unsigned int line_number = 0;
+	char *line_buffer = NULL;
+	char *opcode_string = NULL;
+	size_t line_buffer_size = 0;
+	stack_t *top = NULL;
+	void (*function_pointer)(stack_t **, unsigned int);
+
+	if (argc != 2)
+	{
+		fprintf(stderr, "USAGE: Monty file\n");
+		exit(EXIT_FAILURE);
+	}
+
+	FILE *monty_file = fopen(argv[1], "r");
+	if (monty_file == NULL)
+	{
+		fprintf(stderr, "Error: Can't open file %s\n", argv[1]);
+		exit(EXIT_FAILURE);
+	}
+
+	while (getline(&line_buffer, &line_buffer_size, monty_file) != -1)
+	{
+		line_number++;
+		opcode_string = strtok(line_buffer, " \n\t");
+
+		if (opcode_string == NULL || strcmp(opcode_string, "\n") == 0)
+				continue;
+
+		function_pointer = execute(opcode_string);
+
+		if (function_pointer == NULL)
+		{
+			fprintf(stderr, "Line %u: Unknown instruction %s\n", line_number, opcode_sring);
+			exit(EXIT_FAILURE);
+		}
+
+		function_pointer(&top, line_number);
+	}
+	free(line_buffer);
+	fclose(monty_file);
+	free_all(top);
+
+	return (EXIT_SUCCESS);
+}
+
+
+		
