39e64954526a950d668ef03e2eb912ce7640d2a4
“cpmoore0317” <kingpkayy@gmail.com>
2023-08-10 12:29:55 -0700

    Removing commented lines in main that were used for testing

--- main.c
+++ main.c
@@ -11,13 +11,6 @@
 
 int main(int argc, char **argv)
 {
-	/*unsigned int line_number = 0;
-	char *line_buffer = NULL;
-	char *opcode_string = NULL;
-	size_t line_buffer_size = 0;
-	stack_t *top = NULL;
-	void (*function_pointer)(stack_t **, unsigned int);*/
-
 	if (argc != 2)
 	{
 		fprintf(stderr, "USAGE: monty file\n");
@@ -34,27 +27,7 @@ int main(int argc, char **argv)
 
 	process_file(monty_file);
 
-	/*while (getline(&line_buffer, &line_buffer_size, monty_file) != -1)
-	{
-		line_number++;
-		opcode_string = strtok(line_buffer, DELIM);
-
-		if (opcode_string == NULL || strcmp(opcode_string, "\n") == 0)
-			continue;
-
-		function_pointer = exec_opcode_func(opcode_string);
-
-		if (function_pointer == NULL)
-		{
-			fprintf(stderr, "L%u: unknown instruction %s\n", line_number, opcode_string);
-			exit(EXIT_FAILURE);
-		}
-
-		function_pointer(&top, line_number);
-	}
-	free(line_buffer);*/
 	fclose(monty_file);
-	/*free_stack(top);*/
 
 	return (EXIT_SUCCESS);
 }
